88dffd5d6c949b6df1579c3ad9ee245ba663b197
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-03-20 11:06:18 +0100

    feat[backend]: enhance API with dotenv integration, logging setup, and modular structure for environment management

--- api/src/bin/main.rs
+++ api/src/bin/main.rs
@@ -1,6 +1,30 @@
+use std::sync::Arc;
+
+use authcrux::env::{AppEnv, Env};
+use clap::Parser;
+
+fn init_logger(env: Arc<Env>) {
+    match env.env {
+        AppEnv::Development => {
+            tracing_subscriber::fmt::init();
+        }
+        AppEnv::Production => {
+            tracing_subscriber::fmt()
+                .json()
+                .with_max_level(tracing::Level::INFO)
+                .init();
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-  println!("Hello AuthCrux");
+    dotenv::dotenv().ok();
 
-  Ok(())
+    let env = Arc::new(Env::parse());
+    init_logger(Arc::clone(&env));
+    
+    println!("Hello AuthCrux");
+
+    Ok(())
 }
